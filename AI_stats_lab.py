db2b51bb57b72382ae17e1e30844708cbb423af0
SALMAN-107 <salmangillsgd@gmail.com>
2026-04-24 11:39:37 +0500

    Add joint CDF and PMF calculations
    Implemented joint CDF and PMF functions for unit square and heads.

--- AI_stats_lab.py
+++ AI_stats_lab.py
@@ -1,9 +1,4 @@
 
----
-
-## `AI_stats_lab.py`
-
-```python
 import numpy as np
 
 
@@ -22,7 +17,19 @@ def joint_cdf_unit_square(x, y):
         y                   if x >= 1 and 0 < y < 1
         1                   if x >= 1 and y >= 1
     """
-    pass
+    if x <= 0 or y <= 0:
+        return 0
+    elif 0 < x < 1 and 0 < y < 1:
+        return x * y
+    elif 0 < x < 1 and y >= 1:
+        return x
+    elif x >= 1 and 0 < y < 1:
+        return y
+    elif x >= 1 and y >= 1:
+        return 1
+    else:
+        # This shouldn't happen, but just in case
+        return 0
 
 
 def rectangle_probability(x1, x2, y1, y2):
@@ -30,7 +37,10 @@ def rectangle_probability(x1, x2, y1, y2):
     Compute P(x1 < X <= x2, y1 < Y <= y2)
     using the joint CDF rectangle formula.
     """
-    pass
+    return (joint_cdf_unit_square(x2, y2) - 
+            joint_cdf_unit_square(x1, y2) - 
+            joint_cdf_unit_square(x2, y1) + 
+            joint_cdf_unit_square(x1, y1))
 
 
 def marginal_fx_unit_square(x):
@@ -41,7 +51,10 @@ def marginal_fx_unit_square(x):
         1   if 0 < x < 1
         0   otherwise
     """
-    pass
+    if 0 < x < 1:
+        return 1
+    else:
+        return 0
 
 
 def marginal_fy_unit_square(y):
@@ -52,7 +65,10 @@ def marginal_fy_unit_square(y):
         1   if 0 < y < 1
         0   otherwise
     """
-    pass
+    if 0 < y < 1:
+        return 1
+    else:
+        return 0
 
 
 # -------------------------------------------------
@@ -70,25 +86,55 @@ def joint_pmf_heads(x, y):
         x=0      1/4   1/4    0
         x=1       0    1/4   1/4
     """
-    pass
+    if x == 0 and y == 0:
+        return 1/4
+    elif x == 0 and y == 1:
+        return 1/4
+    elif x == 0 and y == 2:
+        return 0
+    elif x == 1 and y == 0:
+        return 0
+    elif x == 1 and y == 1:
+        return 1/4
+    elif x == 1 and y == 2:
+        return 1/4
+    else:
+        return 0
 
 
 def marginal_px_heads(x):
     """
     Return P_X(x) by summing the joint PMF over y.
     """
-    pass
+    if x == 0:
+        return joint_pmf_heads(0, 0) + joint_pmf_heads(0, 1) + joint_pmf_heads(0, 2)
+    elif x == 1:
+        return joint_pmf_heads(1, 0) + joint_pmf_heads(1, 1) + joint_pmf_heads(1, 2)
+    else:
+        return 0
 
 
 def marginal_py_heads(y):
     """
     Return P_Y(y) by summing the joint PMF over x.
     """
-    pass
+    if y == 0:
+        return joint_pmf_heads(0, 0) + joint_pmf_heads(1, 0)
+    elif y == 1:
+        return joint_pmf_heads(0, 1) + joint_pmf_heads(1, 1)
+    elif y == 2:
+        return joint_pmf_heads(0, 2) + joint_pmf_heads(1, 2)
+    else:
+        return 0
 
 
 def check_independence_heads():
     """
     Return True if X and Y are independent, else False.
     """
-    pass
+    # Check for all possible x, y if P(x,y) == P(x) * P(y)
+    for x in [0, 1]:
+        for y in [0, 1, 2]:
+            if joint_pmf_heads(x, y) != marginal_px_heads(x) * marginal_py_heads(y):
+                return False
+    return True
